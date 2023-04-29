4b9d5168ff91b72becef89e62b93205bfa41d9f0
Mr1011 <furkan.imamoglu@tum.de>
2023-04-29 22:12:10 +0200

    chat2

--- client/src/pages/Visual.js
+++ client/src/pages/Visual.js
@@ -2,6 +2,7 @@ import Entity from '../components/Entity';
 import { useEffect, useState, useContext } from 'react';
 import MessageContext from '../MessageContext';
 import SummaryContext from '../SummaryContext';
+import Input from '../components/Input';
 
 function Visual() {
   const [summary, setSummary] = useContext(SummaryContext);
@@ -34,13 +35,39 @@ function Visual() {
     fetchData();
   }, []);
 
+  const onSendMessage = async (message) => {
+    setIsLoading(true);
+    setMessages([
+      // spread operator which appends the new message to the end of the array
+
+    ]);
+  }
+
+
   const [entities, setEntities] = useState([]);
 
   return (
-    <div>
-      {entities.map(entity => (
-        <Entity key={entity.name} name={entity.name} description={entity.description} />
+    <div style={{ position: "relative", minHeight: "100vh" }}>
+      {entities.map((entity) => (
+        <Entity
+          key={entity.name}
+          name={entity.name}
+          description={entity.description}
+        />
       ))}
+      <div
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          margin: "0 auto",
+          width: 1000,
+          paddingBottom: 40,
+        }}
+      >
+        <Input onSendMessage={onSendMessage} disabled={isLoading} />
+      </div>
     </div>
   );
 }
