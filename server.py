a829b427676205352b42a63ed7d6c6db343a9aa1
Joong-Won Seo <joong-won.seo@tum.de>
2023-04-29 16:10:35 +0200

    server initial mocked and dev

--- server.py
+++ server.py
@@ -7,37 +7,63 @@ model = 'gpt-3.5-turbo'
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
+# prompts
+INTERVIEWER_PROMPT = """You are an interviewer that asks the user about their job, including title, description, industry, involved parties, goal, etc., in order to understand the role and its goal. You can ask specific questions to clarify."""
+
+
 app = Flask(__name__)
 
-
+# API ENDPOINTS: DEBUG
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 @app.route('/hello_gpt')
 def hello_gpt():
-    prompt = 'Say hi!'
-    response = openai.ChatCompletion.create(
-        engine=model,
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": "Hello!"}
+    ]
+    )
+    print(completion.choices[0].message)
+    return jsonify(completion.choices[0].message)
+
+
+# API ENDPOINTS: MOCKED
+@app.route('/interviewer', methods=['GET', 'POST'])
+def interviewer():
+    # get the conversation from the request
+    input = request.get_json()
+
+    conversation = input['conversation']
+    system_prompt = input.get('system_prompt', INTERVIEWER_PROMPT)
+
+    return "Oh that's great! Thank you for your time."
+
+
+
+
+# API ENDPOINTS: REAL
+@app.route('/dev_interviewer', methods=['POST'])
+def dev_interviewer():
+    # get the conversation from the request
+    input = request.get_json()
+
+    conversation = input['conversation']
+    system_prompt = input.get('system_prompt', "You are a helpful assistant.")
+
+    # send the conversation to GPT
+    return ask_chatGPT(conversation, system_prompt)
+
+
+# HELPER FUCTIONS
+def ask_chatGPT(conversation, system_prompt="You are a helpful assistant."):
+    completion = openai.ChatCompletion.create(
+        model=model,
         messages=[
-            {'role': 'user', 'content': prompt},
-        ]
+            {'role': 'system', 'content': system_prompt},
+        ] + conversation,
     )
-    return jsonify(response)
-
-
-@app.route('/api/gpt3', methods=['POST'])
-def gpt3():
-    data = request.get_json()
-    prompt = data['prompt']
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=100,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop=["\n", " Human:", " AI:"]
-    )
-    return jsonify(response)
+    print('DEBUG:', completion.choices[0].message)
+    return jsonify(completion.choices[0].message.content)
