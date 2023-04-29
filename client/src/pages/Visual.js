262a541af268f67e8808c9c567b4edd0d3f94143
Woojin Ko <woojinko@Woojins-MacBook-Air.local>
2023-04-29 22:00:35 +0200

    2chat

--- client/src/pages/Visual.js
+++ client/src/pages/Visual.js
@@ -1,49 +1,45 @@
-import Entity from '../components/Entity';
-import { useEffect, useState, useContext } from 'react';
-import MessageContext from '../MessageContext';
+import Entity from "../components/Entity";
+import { useEffect, useState, useContext } from "react";
+import MessageContext from "../MessageContext";
+
+import Input from "../components/Input";
 
 const getEntities = async () => {
-  const response = await fetch('http://10.183.68.9:5000/', {
-    method: 'GET',
+  const response = await fetch("http://10.183.68.9:5000/", {
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-  const data = await response.json()
-  console.log(data.entities)
-  return data.entities
-}
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+  console.log(data.entities);
+  return data.entities;
+};
 
 // create a post request that sends data to here: http://10.183.68.9:5000/ and then awaits a response
 // the response should be the data that is sent back from the server
 
 const postEntities = async () => {
-  const response = await fetch('http://10.183.68.9:5000/', {
-    method: 'POST',
+  const response = await fetch("http://10.183.68.9:5000/", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       // Chata data
-    })
-  })
-  const data = await response.json()
-  console.log(data.entities)
-  return data.entities
-}
-
-
-
+    }),
+  });
+  const data = await response.json();
+  console.log(data.entities);
+  return data.entities;
+};
 
 const mockData = {
-  "entities": [
-    { "name": "ENTITY1", "description": "DESCRIPTION 1" },
+  entities: [{ name: "ENTITY1", description: "DESCRIPTION 1" }],
+  relations: [
+    { from: "ENTITY1", to: "ENTITY2", description: "RELATION DESCRIPTION 1" },
   ],
-  "relations": [
-    { "from": "ENTITY1", "to": "ENTITY2", "description": "RELATION DESCRIPTION 1" },
-  ]
-}
-
+};
 
 function Visual() {
   const [entities, setEntities] = useState([]);
@@ -53,16 +49,43 @@ function Visual() {
   //   getEntities().then(data => setEntities(Object.entries(data).map(([name, description]) => ({ name, description }))));
   // }, []);
 
-
   useEffect(() => {
     setEntities(mockData.entities);
   }, []);
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSendMessage = async (message) => {
+    console.log(messages);
+    setIsLoading(true);
+
+    // API POST and GET requests
+
+    setIsLoading(false);
+  };
+
   return (
-    <div >
-      {entities.map(entity => (
-        <Entity key={entity.name} name={entity.name} description={entity.description} />
+    <div style={{ position: "relative", minHeight: "100vh" }}>
+      {entities.map((entity) => (
+        <Entity
+          key={entity.name}
+          name={entity.name}
+          description={entity.description}
+        />
       ))}
+      <div
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          margin: "0 auto",
+          width: 1000,
+          paddingBottom: 40,
+        }}
+      >
+        <Input onSendMessage={onSendMessage} disabled={isLoading} />
+      </div>
     </div>
   );
 }
