db884354384e97f06990e19ec2a15ff5702faa63
Woojin Ko <woojinko@Woojins-MacBook-Air.local>
2023-04-30 03:36:16 +0200

    speech

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,7 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import * as sdk from "microsoft-cognitiveservices-speech-sdk";
+// .env
+import dotenv from "dotenv";
+dotenv.config();
 
 const Input = ({ onSendMessage, disabled }) => {
   const [text, setText] = useState("");
+  const [isRecording, setIsRecording] = useState(false);
+  const [sendClicked, setSendClicked] = useState(false);
+  const [recognizer, setRecognizer] = useState(null);
+
+  const handleVoiceRecognition = () => {
+    setIsRecording(true);
+
+    // Creates an instance of a speech config with specified subscription key and service region.
+    const speechConfig = sdk.SpeechConfig.fromSubscription(
+      process.env.REACT_APP_AZURE_KEY,
+      "germanywestcentral"
+    );
+    const audioConfig = sdk.AudioConfig.fromDefaultMicrophoneInput();
+    const recognizerInstance = new sdk.SpeechRecognizer(
+      speechConfig,
+      audioConfig
+    );
+    setRecognizer(recognizerInstance);
+
+    recognizerInstance.recognizing = (s, e) => {
+      setText(text + " " + e.result.text);
+    };
+
+    recognizerInstance.recognizeOnceAsync(
+      (result) => {
+        setText(text + " " + result.text);
+        setIsRecording(false);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    // Change the button text to "Stop" while recording
+    const speakButton = document.getElementById("speak-button");
+    if (speakButton) {
+      speakButton.textContent = "Stop";
+    }
+  };
+
+  const handleStopRecording = () => {
+    setIsRecording(false);
+
+    if (!recognizer) {
+      return;
+    }
+
+    recognizer.stopContinuousRecognitionAsync();
+
+    // Reset the button text to "Speak" after recording stops
+    const speakButton = document.getElementById("speak-button");
+    if (speakButton) {
+      speakButton.textContent = "Speak";
+    }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,6 +73,14 @@ const Input = ({ onSendMessage, disabled }) => {
     setText(e.target.value);
   };
 
+  const handleSendClick = () => {
+    setSendClicked(true);
+    if (!isRecording && text.trim() !== "") {
+      onSendMessage(text);
+      setText("");
+    }
+  };
+
   return (
     <div className="Input">
       <form onSubmit={handleSubmit}>
@@ -23,9 +90,18 @@ const Input = ({ onSendMessage, disabled }) => {
           type="text"
           placeholder="Enter your message and press ENTER"
           autoFocus
-          disabled={disabled}
+          disabled={isRecording || disabled}
         />
-        <button>Send</button>
+        <button
+          id="speak-button"
+          type="button"
+          onClick={isRecording ? handleStopRecording : handleVoiceRecognition}
+        >
+          {isRecording ? "Stop" : "Speak"}
+        </button>
+        <button type="button" onClick={handleSendClick}>
+          Send
+        </button>
       </form>
     </div>
   );
